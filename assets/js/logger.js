5122542581bf7b2e470831ee0e4e54acc9cb28fd
morehwachege <antony123muriithi@gmail.com>
2022-03-16 19:49:31 +0300

    arrange structure of files

---
+++ assets/js/logger.js
@@ -0,0 +1,10 @@
+var url = 'http://mylogger.io/log';
+
+function log(message){
+    //  send an https request
+    console.log(message);
+}
+
+
+module.exports =  logging, loggest;
+// module.exports.url = url;
