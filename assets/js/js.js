042cd585a723624115e917be7956b90fcdb0739d
morehwachege <antony123muriithi@gmail.com>
2022-03-17 23:36:34 +0300

    canvas events listeners

--- assets/js/js.js
+++ assets/js/js.js
@@ -53,6 +53,76 @@ class Platform{
 }
 
 
+const player = new Player();
+const platform = new Platform();
+
+
+const keys = {
+    right: {
+        pressed: false,
+    },
+    left: {
+        pressed: false,
+    }
+}
+function animate(){
+    requestAnimationFrame(animate);
+    c.clearRect(0,0, canvas.width, canvas.height)
+    player.update();
+    platform.draw();
+    if( keys.right.pressed ){
+        player.velocity.x = 5;
+    }
+    else if( keys.left.pressed ){
+        player.velocity.x = -5;
+    }else player.velocity.x = 0;
+}
+animate()
+
+addEventListener('keydown', ({ keyCode }) =>{
+    // console.log(keyCode)
+    switch( keyCode ){
+        case 65:
+            console.log('left')
+            keys.left.pressed = true;
+            break
+        case 83:
+            console.log('down')
+            break
+        case 68:
+            console.log('right');
+            // player.velocity.x += 2;
+            keys.right.pressed = true;
+            break;
+        case 87:
+            console.log('up');
+            player.velocity.y -= 20;
+            break;
+    }
+})
+addEventListener('keyup', ({ keyCode }) =>{
+    // console.log(keyCode)
+    switch( keyCode ){
+        case 65:
+            console.log('left')
+            keys.left.pressed = false;
+            break
+        case 83:
+            console.log('down')
+            break
+        case 68:
+            console.log('right');
+            keys.right.pressed = false;
+            break;
+        case 87:
+            console.log('up');
+            player.velocity.y -= 20;
+            break;
+    }
+})
+
+
+
 
 
 
